5df99abd52dd03239016fef00106a3d61e7afddb
Genevieve Perron Migneron <info@gen-migneron.com>
2022-04-15 11:59:38 -0400

    Build simple header

--- src/layouts/header/index.jsx
+++ src/layouts/header/index.jsx
@@ -3,9 +3,9 @@ import './styles.sass';
 
 function Header() {
   return (
-    <header className="header d-lg-block d-none">
-      <div className="row align-items-center">
-        <div className="col-lg-10 col-xl-8">
+    <header className="header">
+      <div className="row">
+        <div className="col-md-12">
           <img className="header__logo" src="/images/logo.png" alt="Mario Party Super Star Logo" />
         </div>
       </div>
